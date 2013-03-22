4bb1dd35c13762674a418ef1f9582910a4482c70
Fabian Imasand <fabian.imsand@hotmail.com>
2013-03-22 16:26:06 +0100

    Starting Project

---
+++ HolidayApartment/src/ch/holidayaparment/server/GreetingServiceImpl.java
@@ -0,0 +1,13 @@
+package ch.holidayaparment.server;
+
+import ch.holidayaparment.client.GreetingService;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class GreetingServiceImpl extends RemoteServiceServlet implements
+		GreetingService {
+
+}
